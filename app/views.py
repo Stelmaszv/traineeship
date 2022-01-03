af6951e47a4536b4a6d186d64cf42bf9d749bba0
= <stelmaszv@gmail.com>
2022-01-03 12:52:59 +0100

    Add add_get method to class "AbstractView" (view.py)

--- app/views.py
+++ app/views.py
@@ -1,7 +1,17 @@
+from django.shortcuts import render
 from django.views.generic import TemplateView
 
 class AbstractView(TemplateView):
+
     template_name = "main.html"
+    context = {}
+    def add_get(self, request, *args, **kwargs):
+        self.request = request
+        return render(request, self.template_name, self.context)
+
+    def get(self, request, *args, **kwargs):
+        return self.add_get(request)
 
 class KilometresPerHourViev(TemplateView):
     template_name = "KilometresPerHour.html"
+
