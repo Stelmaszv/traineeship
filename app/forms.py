ba53b4dfe46d2e57f8bfb1d76bfab3c6d57b9722
= <stelmaszv@gmail.com>
2022-01-03 15:49:03 +0100

    Add coments to console version

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,4 @@
 from django import forms
-from django.core.exceptions import ValidationError
-
 
 class Calculate(forms.Form):
     value = forms.IntegerField()
--- console.py
+++ console.py
@@ -6,13 +6,18 @@ while run ==False:
     print("2 For counting Kilometres Per Hour to Foot Per Hour")
     value=input("Please set Type of couting :")
     if int(value)  == 1 or int(value) == 2:
+        #seting type of couting
         objets={
             1:KilometresPerHourCounter,
             2:FootPerHourCounter
         }
+        # set counter
         object=objets.get(int(value))
+        # enter value
         value_for_counting = input("Please enter value:")
+        # counting
         data=object().count(int(value_for_counting))
         print(data.result)
+        # show if speed of sound
         if data.speed_of_sound:
             print('Warning speed of sound !')
