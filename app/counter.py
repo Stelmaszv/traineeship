e8a9ca7880a5e96ef1cd0ed08b35abd6f55d7821
= <stelmaszv@gmail.com>
2022-01-03 15:12:57 +0100

    Add coments to web app

--- app/counter.py
+++ app/counter.py
@@ -2,13 +2,13 @@ from abc import ABC, abstractmethod
 
 class AbstractCounter(ABC):
 
-    speed_of_sound_limit=0
-    speed_of_sound=False
-    result=0
+    speed_of_sound_limit=0   #speed of soud value
+    speed_of_sound=False     #if value is greater than speed of soud value
+    result=0                 #result of couting
 
     def count(self,value):
-        self.on_count(value)
-        self.if_speed_of_soud(value)
+        self.on_count(value)          #abstract method on count
+        self.if_speed_of_soud(value)  #chceck if value is greater than speed of soud value
         return self
 
     @abstractmethod
@@ -17,22 +17,18 @@ class AbstractCounter(ABC):
 
     def if_speed_of_soud(self,value):
         if value>self.speed_of_sound_limit:
-            self.speed_of_sound=True
+            self.speed_of_sound=True #if value is greater than speed of soud value set true
 
 class KilometresPerHourCounter(AbstractCounter):
 
-    speed_of_sound=False
-    result=0
-    speed_of_sound_limit=1234.8
+    speed_of_sound_limit=1234.8 #speed of soud value
 
     def on_count(self,value):
-        self.result=value * 3280.840
+        self.result=value * 3280.840 #counting Foot Per Hour to Kilometres Per Hour
 
 class FootPerHourCounter(AbstractCounter):
 
-    speed_of_sound=False
-    result=0
-    speed_of_sound_limit=4051181
+    speed_of_sound_limit=4051181 #speed of soud value
 
     def on_count(self,value):
-        self.result=value / 3280.840
+        self.result=value / 3280.840 #counting Kilometres Per Hour to Foot Per Hour
--- app/views.py
+++ app/views.py
@@ -5,36 +5,36 @@ from .forms import Calculate
 
 class AbstractView(TemplateView):
 
-    template_name = "main.html"           #set_templete
-    context = {"form": Calculate()}       #data for templete
-    Counter=None                          #instance of counter
+    template_name = "main.html"                                                               #set_templete
+    context = {"form": Calculate()}                                                           #data for templete
+    Counter=None                                                                              #instance of counter
 
     def get_data(self,request):
         self.Counter() #get_counter
         form = Calculate(request.POST or None)
         if form.is_valid():
-            counter=self.Counter().count(form.cleaned_data.get('value'))
-            self.context = {"Counter": counter, "form": Calculate()}
-            return render(request, self.template_name, self.context)
-        return render(request, self.template_name, self.context)
+            counter=self.Counter().count(form.cleaned_data.get('value'))                      #set data for couter
+            self.context = {"Counter": counter, "form": Calculate()}                          #set data for view
+            return render(request, self.template_name, self.context)                          #return view
+        return render(request, self.template_name, self.context)                              #return view
 
     def add_post(self,request):
         return self.get_data(request)
 
     def post(self, request, *args, **kwargs):
-        return self.add_post(request) #action in post
+        return self.add_post(request)                                                       #action in post
 
     def add_get(self, request, *args, **kwargs):
         return self.get_data(request)
 
     def get(self, request, *args, **kwargs):
-        return self.add_get(request) #action in get
+        return self.add_get(request)                                                         #action in get
 
 class KilometresPerHourViev(AbstractView):
-    template_name = "KilometresPerHour.html"    #set_templete
-    Counter=KilometresPerHourCounter            #instance of counter
+    template_name = "KilometresPerHour.html"                                                #set_templete
+    Counter=KilometresPerHourCounter                                                        #instance of counter
 
 class FootPerHourViev(AbstractView):
-    template_name = "FootPerHour.html"          #set_templete
-    Counter=FootPerHourCounter                  #instance of counter
+    template_name = "FootPerHour.html"                                                      #set_templete
+    Counter=FootPerHourCounter                                                              #instance of counter
 
