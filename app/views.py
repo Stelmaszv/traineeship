58926184e5b2de8cf7731d0c48f6398bb54c1efb
= <stelmaszv@gmail.com>
2022-01-03 12:34:52 +0100

    Add view KilometresPerHourViev (view.py)

--- app/views.py
+++ app/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
 from django.views.generic import TemplateView
 
 class AbstractView(TemplateView):
     template_name = "main.html"
+
+class KilometresPerHourViev(TemplateView):
+    template_name = "KilometresPerHour.html"
--- traineeship/urls.py
+++ traineeship/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from app.views import AbstractView
+from app.views import AbstractView,KilometresPerHourViev
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('main/',AbstractView.as_view())
+    path('main/',AbstractView.as_view()),
+    path('kilometresperhourviev/',KilometresPerHourViev.as_view())
 ]
