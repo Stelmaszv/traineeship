c62f170499224cb875aae1a818532b7585f88e93
= <stelmaszv@gmail.com>
2022-01-03 14:49:47 +0100

    Update in counter "KilometresPerHourCounter" in method "count"

--- app/counter.py
+++ app/counter.py
@@ -11,9 +11,10 @@ class KilometresPerHourCounter(AbstractCounter):
 
     speed_of_sound=False
     result=0
+    speed_of_sound_limit=1234.8
 
     def count(self,value):
-        self.result=value
-        if self.result>121:
+        self.result=value * 3280.840
+        if value>self.speed_of_sound_limit:
             self.speed_of_sound=True
         return self
